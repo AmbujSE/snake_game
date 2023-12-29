da179af5223d08469c7e2a41ae2709826d7b06fe
AmbujSE <blastedaway94@gmail.com>
2023-12-29 13:02:17 +0530

    updated with highscore feature

--- main.py
+++ main.py
@@ -33,14 +33,13 @@ while game_is_on:
 
     # Collision with wall.
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()
-
+        scoreboard.reset()
+        snake.reset()
     # Collision with tail.
     # if the head collide with any segments in the tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
